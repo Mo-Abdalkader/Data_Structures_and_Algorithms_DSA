01a65b8137ea63d18c0b4f157d81ae88f1cab69c
Mohamed Abdalkader <101468654+Mo-Abdalkader@users.noreply.github.com>
2024-02-01 20:42:21 +0200

    Add files via upload

---
+++ Data Structures/Stack/ArrayBasedStack.java
@@ -0,0 +1,71 @@
+package datastructure;
+
+/**
+ *
+ * @author Mohamed Abdalkader
+ */
+public class ArrayBasedStack<T> {
+
+    private int top;
+    private int DEFAULT_SIZE = 10;
+    private Object[] items;
+
+    public ArrayBasedStack() {
+        top = -1;
+        items = new Object[DEFAULT_SIZE];
+    }
+
+    public boolean isEmpty() {
+        return top == -1;
+    }
+
+    public void clear() {
+        top = -1;
+        items = new Object[DEFAULT_SIZE];
+    }
+
+    public int size() {
+        return top + 1;
+    }
+
+    public T pop() {
+        if (!isEmpty()) { // To avoid errors, if the stack is already empty
+            T itemToPop = (T) items[top]; // Get the top element to be returned
+            items[top] = null; // Remove this element
+            top--;
+            return itemToPop; // Return the element I saved before
+        }
+        return null;
+    }
+
+    public void enlarge() {
+        Object[] new_array = new Object[items.length * 2];
+        System.arraycopy(items, 0, new_array, 0, items.length);
+        items = new_array;
+    }
+
+    public void push(T item) {
+        if (top == items.length - 1) { // If the Stack is full, Enlarging the stack is needed
+            enlarge();
+        }
+        items[++top] = item;
+    }
+
+    public void display() {
+        for (int i = 0; i <= top; i++) {
+            System.out.println(items[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        ArrayBasedStack<Integer> stack = new ArrayBasedStack<>();
+        stack.push(1); // Stack = [1]
+        stack.push(2); // Stack = [1, 2]
+        stack.push(3); // Stack = [1, 2, 3]
+        System.out.println(stack.pop()); // Output = 3, Stack = [1, 2]
+        System.out.println("");
+        stack.push(4); // Stack = [1, 2, 4]
+        System.out.println(stack.size()); // 3
+        stack.display();
+    }
+}
